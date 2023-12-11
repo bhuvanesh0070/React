d440fd02463d0d3fd7823ab9d0e4ed2ba9237c54
Bhuvanesh <125265783+bhuvanesh0070@users.noreply.github.com>
2023-12-11 15:52:19 +0530

    added day3

---
+++ React/Class Work/day3/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import ClassComponents from './component/ClassComponent';
+import FunctionalComponent from './component/functionalComponent';
+function App() {
+  return (
+    <div>
+      <ClassComponents>
+
+      </ClassComponents>
+      <FunctionalComponent></FunctionalComponent>
+    </div>
+  )
+}
+
+export default App;
---
+++ React/Class Work/day3/src/component/ClassComponent.js
@@ -0,0 +1,11 @@
+import { Component }from "react";
+export default class ClassComponent extends Component{
+    render(){
+        return(
+          <div>
+          <p>Hello world from class Component</p>
+          <p>Another tag</p>
+          </div>
+        )
+    }
+}
